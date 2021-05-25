dae89e154022973a78eced436633b4ff9e49266d
RathodSooraj <83805520+RathodSooraj@users.noreply.github.com>
2021-05-25 17:57:05 +0530

    Base Structure Uploaded

---
+++ CTGeneralHospital/src/main/java/com/ctgeneralhospital/CtGeneralHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.ctgeneralhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtGeneralHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtGeneralHospitalApplication.class, args);
+	}
+
+}
---
+++ CTGeneralHospital/src/test/java/com/ctgeneralhospital/CtGeneralHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctgeneralhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtGeneralHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
